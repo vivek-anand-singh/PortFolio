c5e9922d81d9e1fe19d09a6bfdb6c1ceba1b80b0
Vivek Anand Singh <17vivekanandsingh@gmail.com>
2024-06-03 00:55:35 +0530

    Small Update

--- src/Components/About/description.js
+++ src/Components/About/description.js
@@ -1,22 +1,12 @@
 import './description.css';
-import Photo from '../../Assets/photo.svg';
+import Photo from './myphoto.jpeg';
 
-function Description()
-{
-    return(
+function Description() {
+    return (
         <div id="description">
             <div id="description-text">
                 <h3>
-                    This is where you can describe about yourself. 
-                    The more you describe about yourself, the better it is!
-                </h3>
-                <h3>
-                    Extra Information about you, like hobbies and your goals.
-                    Example Paragraph: I am passionate about my work. Because I love what I do,
-                    I have a steady source of motivation that drives me to do my best. 
-                    In my last job, this passion led me to challenge myself daily and learn new skills that helped me to do better work. 
-                    For example, I taught myself how to use Photoshop to improve the quality of our photos and graphics.
-                    I soon became the go-to person for any design needs.
+                    I'm an adventurous soul, always seeking new challenges and experiences. I thrive on the excitement of discovery. My curiosity extends to learning about diverse cultures and histories, delving into captivating narratives that broaden my perspective. Photography is a significant passion of mine, where I freeze fleeting moments of beauty and intrigue through my lens, sharing my unique viewpoint with the world. Professionally, I'm committed to environmental science, dedicated to finding sustainable solutions to global challenges, be it researching renewable energy or advocating for conservation efforts. I approach life with a blend of curiosity and purpose, believing in making a positive impact by following my passions and seizing every opportunity to leave a meaningful mark on the world.
                 </h3>
             </div>
             <div id="description-image">
